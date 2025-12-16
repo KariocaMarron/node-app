56735bba19d96dc74b2daa7ac7372290f8df4f02
Karioca Marron <5bataj10@solent.ac.uk>
2025-12-16 00:02:06 +0000

    Add Express web server with static site

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-console.log('Node.js app is running');
+const express = require('express'); const path = require('path'); const app = express(); const PORT = 3000; app.use(express.static(path.join(__dirname, 'public'))); app.listen(PORT, () => console.log('Web server running on port ' + PORT));
